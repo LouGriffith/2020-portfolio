a0c18b5b0b5e141a9ebaf839e47bf7782ffe75cf
Lou Griffith <lou.griffith@stjude.org>
2020-07-05 23:23:51 -0500

    Created switch

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
-function Hi() {
-  return <div>Hello <strong>Lou</strong></div>;
+function Room() {
+  const [isLit, setLit] = useState(true);
+
+  return (
+    <div className="room">
+      the room is {isLit ? 'lit' : 'dark'}
+      <br />
+      <button onClick={() => setLit(!isLit)}>
+        flip
+      </button>
+    </div>
+  );
 }
 
-ReactDOM.render(<Hi/>, document.querySelector('#root'));
+ReactDOM.render(<Room />, document.getElementById('root'));
