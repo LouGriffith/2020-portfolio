a75b15780b3a2fa6d271bb2bd18a9cc101bc393b
Lou Griffith <lou.griffith@stjude.org>
2020-07-05 23:33:33 -0500

    Optimized lesson code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 function Room() {
   const [isLit, setLit] = useState(true);
+  const brightness = isLit ? "lit" : "dark";
 
   return (
-    <div className="room">
+    <div className={`room ${isLit ? 'lit' : 'dark'}`}>
       the room is {isLit ? 'lit' : 'dark'}
       <br />
       <button onClick={() => setLit(!isLit)}>
